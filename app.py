7b0fc09ba52ffff568716fde0e4cc845df87ad60
aoluwatooni <53844337+aoluwatooni@users.noreply.github.com>
2020-05-10 08:38:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,10 +31,7 @@ def predict():
 
 
 if __name__ == '__main__':
-    try:
-        port = int(sys.argv[1])  # This is for a command-line input
-    except BaseException:
-        port = 12345  # If you don't provide any port the port will be set to 12345
+
 
     regr = joblib.load("model.pkl")  # Load "model.pkl"
     print('Model loaded')
