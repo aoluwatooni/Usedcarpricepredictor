1d126aafa6aa692e12b74d4b8b01ff9fb7476bdc
aoluwatooni <53844337+aoluwatooni@users.noreply.github.com>
2020-05-10 10:32:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import joblib
 import traceback
 import pandas as pd
 import numpy as np
+regr = joblib.load("model.pkl")  # Load "model.pkl"
 
 # Your API definition
 app = Flask(__name__)
@@ -11,12 +12,12 @@ app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if joblib.load("model.pkl"):
+    if regr:
         try:
             json_ = request.json
             print(json_)
             query = pd.get_dummies(pd.DataFrame(json_))
-            query = query.reindex(columns=joblib.load("model_columns.pkl"), fill_value=0)
+            query = query.reindex(columns=model_columns, fill_value=0)
 
             prediction = list(regr.predict(query))
 
